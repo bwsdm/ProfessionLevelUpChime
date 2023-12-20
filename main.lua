55abba491dc79f0f25c9f0f939391be4c2ab87e4
bwsdm <bobby.wisdom@gmail.com>
2023-12-20 09:09:17 -0600

    fix(trigger): fixed sound triggering off of regular skills

--- main.lua
+++ main.lua
@@ -6,5 +6,23 @@ end
 
 frame:RegisterEvent("CHAT_MSG_SKILL")
 frame:SetScript("OnEvent", function(self, event, ...)
-	playTrack()
+	local professionList = {
+		"Mining",
+		"Skinning",
+		"Herbalism",
+		"Fishing",
+		"Enchanting",
+		"Leatherworking",
+		"Tailoring",
+		"Engineering",
+		"Blacksmithing",
+		"Cooking",
+		"First Aid",
+		"Alchemy",
+	}
+	for k in ipairs(professionList) do
+		if string.find(arg1, professionList[k]) then
+			playTrack()
+		end
+	end
 end)
